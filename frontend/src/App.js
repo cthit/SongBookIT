1d3e3d28c76cb580f1da9c339c2f0b763cd5fe2f
Eric Carlsson <caeric@student.chalmers.se>
2018-11-20 19:19:16 +0100

    added router etc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path="/" exact>
+            <p> Hej </p>
+          </Route>
+          <Route path="/users/:id" exact render={<p> skjdmksld </p>} />
+        </Switch>
       </div>
     );
   }
