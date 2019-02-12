a4c309de7c914fa0471760da4b83e905d83e572b
Eric Carlsson <caeric@student.chalmers.se>
2019-02-12 19:17:54 +0100

    made digit-react-components work again

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -14,7 +14,7 @@ class App extends Component {
                             <Route path="/" component={Songs} />
                         </Switch>
                     )}
-                    // renderDrawer={() => <div/>}
+                    renderDrawer={null}
                 />
             </div>
         );
