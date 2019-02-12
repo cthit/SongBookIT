bed035403755377e803f6c4d6b9e6041281a47aa
Elias Sundqvist <elias@sundqvist.com>
2019-02-12 18:25:27 +0100

    Add basic backend

---
+++ backend/app.py
@@ -0,0 +1,100 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, abort
+from pony.orm import db_session, ObjectNotFound, select
+from pony.orm.serialization import to_dict
+
+from UUIDEncoder import UUIDEncoder
+from db import Song, Tag
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['RESTFUL_JSON'] = {
+    'cls': UUIDEncoder,
+    'ensure_ascii': False
+}
+
+class SongRes(Resource):
+    @db_session
+    def get(self, song_id):
+        try:
+            song = Song[song_id]
+            return to_dict(song)
+        except ObjectNotFound:
+            return "Unrecognized Song ID"
+
+    @db_session
+    def delete(self, song_id):
+        try:
+            song = Song[song_id]
+            song.delete()
+            return "Song deleted!"
+        except ObjectNotFound:
+            return "Unrecognized Song ID"
+
+    @db_session
+    def put(self, song_id):
+        data = request.get_json(force=True)
+        song = Song[song_id]
+        for key, value in data:
+            song[key] = value
+
+        return to_dict(song)
+
+
+class SongsRes(Resource):
+    @db_session
+    def get(self):
+        return to_dict([*Song.select(lambda s: True)])
+
+    @db_session
+    def post(self):
+        data = request.get_json(force=True)
+        song = Song(**data)
+        return to_dict(song)
+
+
+class TagsRes(Resource):
+    @db_session
+    def get(self):
+        return to_dict([*Tag.select(lambda t: True)])
+
+    @db_session
+    def post(self):
+        data = request.get_json(force=True)
+        tag = Tag(**data)
+        print(to_dict(tag))
+
+
+class TagRes(Resource):
+    @db_session
+    def get(self, tag_id):
+        try:
+            return to_dict(Tag[tag_id])
+        except ObjectNotFound:
+            return "Unknown tag id"
+
+    @db_session
+    def delete(self, tag_id):
+        try:
+            Tag[tag_id].delete()
+            return "Tag successfully deleted!"
+        except ObjectNotFound:
+            return "Unknown tag id"
+
+
+class TagSongsRes(Resource):
+    @db_session
+    def get(self, tag_id):
+        return to_dict(Tag[tag_id].songs)
+
+
+api.add_resource(SongsRes, '/songs')
+api.add_resource(SongRes, '/songs/<string:song_id>')
+
+api.add_resource(TagsRes, '/tags')
+api.add_resource(TagRes, '/tags/<string:tag_id>')
+
+api.add_resource(TagSongsRes, '/songs/tag/<string:tag_id>')
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/md-to-db.py
@@ -0,0 +1,53 @@
+import re
+
+from pony.orm import db_session
+from db import Tag, Song
+
+mdstr: str = open("md/sangbok.md", 'r', encoding="utf-8").read()
+category_indices = [c.start() for c in re.finditer(r"[^\n]+\n=+", mdstr)]
+
+parts = [mdstr[i:j] for i, j in zip(category_indices, category_indices[1:] + [None])]
+
+categories = [re.split(r"\n=+\n", c) for c in parts]
+
+
+def parse_song(song):
+    try:
+        title, rest = re.split("\n-+\n", song)
+        m = re.search(r"\n\*?[mM][eE][lL]:([^\n\*]+)", rest)
+        if m is not None and m.groups()[0] is not None:
+            mel = str.strip(m.groups()[0])
+        else:
+            mel = None
+        a = re.search(r"\n\*?[tT][eE][xX][tT]: ([^\n\*]+)", rest)
+        if a is not None and a.groups()[0] is not None:
+            author = str.strip(a.groups()[0]) if a.groups()[0] is not None else None
+        else:
+            author = "Unknown"
+        return title, mel, author, rest
+    except Exception:
+        a = 2
+
+
+def get_songs(data):
+    song_indices = [c.start() for c in re.finditer(r"[^\n]+\n-+\n", data)]
+    songs = [data[i:j] for i, j in zip(song_indices[:-1], song_indices[1:]) if j - i > 10]
+    res = []
+    for song in songs:
+        title, mel, author, text = parse_song(song)
+        res.append({'title': title, 'melody': mel, 'author': author, 'text': text})
+    return res
+
+
+@db_session
+def add_category(cat, data):
+    t = Tag(name=cat, pretty_name_sv=cat, pretty_name_en=cat)
+    for song in get_songs(data):
+        s = Song(author=song['author'], text=song['text'], tags={t})
+        if song['melody'] is not None:
+            s.melody = song['melody']
+
+
+for cat, data in categories:
+    if str.isspace(cat): continue
+    add_category(cat, data)
