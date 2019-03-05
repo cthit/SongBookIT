54173114d816440e309f516a41aef48dde3882f9
Elias Sundqvist <elias@sundqvist.com>
2019-03-05 20:27:31 +0100

    Add markdown reader and title to ShowSong component

--- frontend/src/use-cases/songs/screens/common-views/show-song/ShowSong.screen.jsx
+++ frontend/src/use-cases/songs/screens/common-views/show-song/ShowSong.screen.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { DigitLayout, DigitDesign } from "@cthit/react-digit-components";
+import {
+    DigitLayout,
+    DigitDesign,
+    DigitMarkdown,
+    DigitText
+} from "@cthit/react-digit-components";
 
 export const ShowSong = ({ author, melody, song_id, tags, text, title }) => (
     <DigitLayout.Fill>
         <DigitDesign.Card minWidth="300px" maxWidth="800px">
             <DigitDesign.CardBody>
                 <Link to={"/" + song_id}>
-                    <p> {title}</p>
+                    <DigitText.Title text={title} />
                 </Link>
-                <p> {text}</p>
-                <p> {melody}</p>
+                <DigitMarkdown markdownSource={text} />;<p> {melody}</p>
                 <p> {author}</p>
             </DigitDesign.CardBody>
         </DigitDesign.Card>
