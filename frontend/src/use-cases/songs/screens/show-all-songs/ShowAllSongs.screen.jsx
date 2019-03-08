cf672f33be85c66fe25d36d70c74307dc5220029
Eric Carlsson <caeric@student.chalmers.se>
2019-03-08 20:34:35 +0100

    minimized show-all-songs

--- frontend/src/use-cases/songs/screens/show-all-songs/ShowAllSongs.screen.jsx
+++ frontend/src/use-cases/songs/screens/show-all-songs/ShowAllSongs.screen.jsx
@@ -1,31 +1,14 @@
 import React from "react";
-import {
-    DigitLayout,
-    DigitFAB,
-    DigitDesign
-} from "@cthit/react-digit-components";
+import { DigitLayout, DigitFAB } from "@cthit/react-digit-components";
 import ShowSong from "../common-views/show-song";
-import { getSongs } from "../../../../api/songs/get.songs.api";
 import Add from "@material-ui/icons/Add";
 import Dialog from "../common-views/dialog-view/";
 
 class ShowAllSongs extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { songs: [], open: false };
-        this.getAllSongs();
+        console.log("props");
     }
-
-    getAllSongs = () => {
-        getSongs()
-            .then(res => {
-                const songData = Object.values(res.data.Song);
-                this.setState({ songs: songData });
-            })
-            .catch(err => {
-                console.log("Error when loading all songs", err);
-            });
-    };
 
     handleClickOpen = () => {
         this.setState({ open: true });
@@ -39,7 +22,7 @@ class ShowAllSongs extends React.Component {
         return (
             <DigitLayout.Fill>
                 <DigitLayout.Column centerHorizontal alignTop padding="10px">
-                    {this.state.songs.map(s => (
+                    {this.props.songs.map(s => (
                         <ShowSong {...s} key={s.song_id} />
                     ))}
                 </DigitLayout.Column>
