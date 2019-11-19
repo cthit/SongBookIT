29b843f315fe550ae5c294476c2cbe178497f0e2
Eric Carlsson <cirecirecire3@gmail.com>
2019-11-19 17:51:52 +0100

    Fixed issues with docker

--- backend/app.py
+++ backend/app.py
@@ -101,4 +101,4 @@ api.add_resource(TagRes, '/tags/<string:tag_id>')
 
 api.add_resource(TagSongsRes, '/songs/tag/<string:tag_id>')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
