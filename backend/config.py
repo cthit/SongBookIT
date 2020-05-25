aed3d587ba525d491128c03140f4300ea9f07581
Theodor Angergård <portalprogramming@gmail.com>
2020-05-25 21:17:08 +0200

    Add @admin_required

--- backend/config.py
+++ backend/config.py
@@ -18,3 +18,4 @@ GAMMA_ME_URI = os.environ.get('GAMMA_ME_URI', 'http://gamma-backend:8081/api/use
 GAMMA_TOKEN_URI = os.environ.get('GAMMA_TOKEN_URI', 'http://gamma-backend:8081/api/oauth/token')
 GAMMA_AUTHORIZATION_URI = os.environ.get('GAMMA_AUTHORIZATION_URI', 'http://localhost:8081/api/oauth/authorize')
 GAMMA_REDIRECT_URI = os.environ.get('GAMMA_REDIRECT_URI', 'http://localhost:3001/auth/account/callback')
+GAMMA_ADMIN_AUTHORITY = os.environ.get('GAMMA_ADMIN_AUTHORITY', 'admin')
