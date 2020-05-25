483030da5b9f61030b1142b65bf32e11c7e6d352
Theodor Angergård <portalprogramming@gmail.com>
2020-05-25 17:39:01 +0200

    Resolve rdc and docker issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
 
-import { DigitDialog, DigitProviders } from "@cthit/react-digit-components";
+import { DigitProviders } from "@cthit/react-digit-components";
 import App from "./app";
 
 ReactDOM.render(
-    <DigitProviders defaultLangauge="sv">
-        <>
-            <DigitDialog />
+    <DigitProviders>
             <App />
-        </>
     </DigitProviders>,
     document.getElementById("root")
 );
-
-registerServiceWorker();
