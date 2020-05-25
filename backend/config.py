24d8fbc7bb6f2164dfb3d51e16e9bc2191c40365
Theodor Angergård <portalprogramming@gmail.com>
2020-05-25 20:58:27 +0200

    Add gamma integration

--- backend/config.py
+++ backend/config.py
@@ -9,3 +9,12 @@ POSTGRES_PASSWORD = os.environ.get('SONGBOOKIT_POSTGRES_PASSWORD', 'password')
 POSTGRES_HOST = os.environ.get('SONGBOOKIT_POSTGRES_HOST', 'localhost')
 POSTGRES_PORT = os.environ.get('SONGBOOKIT_POSTGRES_PORT', '5432')
 POSTGRES_DB = os.environ.get('SONGBOOKIT_POSTGRES_DB', 'postgres')
+
+SECRET_KEY = os.environ.get('SONGBOOKIT_SECRET_KEY', 'secret')
+
+GAMMA_CLIENT_ID = os.environ.get('GAMMA_CLIENT_ID', 'id')
+GAMMA_SECRET = os.environ.get('GAMMA_SECRET', 'secret')
+GAMMA_ME_URI = os.environ.get('GAMMA_ME_URI', 'http://gamma-backend:8081/api/users/me')
+GAMMA_TOKEN_URI = os.environ.get('GAMMA_TOKEN_URI', 'http://gamma-backend:8081/api/oauth/token')
+GAMMA_AUTHORIZATION_URI = os.environ.get('GAMMA_AUTHORIZATION_URI', 'http://localhost:8081/api/oauth/authorize')
+GAMMA_REDIRECT_URI = os.environ.get('GAMMA_REDIRECT_URI', 'http://localhost:3001/auth/account/callback')
--- frontend/src/api/utils/api.js
+++ frontend/src/api/utils/api.js
@@ -4,19 +4,19 @@ import _ from "lodash";
 const path = "http://localhost:5000/";
 
 export function getRequest(endpoint) {
-    return axios.get(removeLastSlash(path + endpoint));
+    return axios.get(removeLastSlash(endpoint));
 }
 
 export function postRequest(endpoint, data) {
-    return axios.post(removeLastSlash(path + endpoint, data));
+    return axios.post(removeLastSlash(endpoint, data));
 }
 
 export function deleteRequest(endpoint) {
-    return axios.delete(removeLastSlash(path + endpoint));
+    return axios.delete(removeLastSlash(endpoint));
 }
 
 export function putRequest(endpoint, data) {
-    return axios.put(removeLastSlash(path + endpoint), data);
+    return axios.put(removeLastSlash(endpoint), data);
 }
 
 function removeLastSlash(path) {
